23590c59f324487e7b0fe5652f764f1020d46de1
arthur tshaka <arthurltshaka@gmail.com>
2021-09-09 10:39:12 +0100

    added services folder for isolated testing

---
+++ CustomerApp_RefactorStarter/NorthwindData/Services/ICustomerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindData.Services
+{
+	public interface ICustomerService
+	{
+		List<Customer> GetCustomers();
+		public Customer GetCustomerById();
+		public void CreateCustomer(Customer c);
+		public void SaveCustomerChanges();
+		public void RemoveCustomer(Customer c);
+
+
+	}
+}
